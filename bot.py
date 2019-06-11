d8c1ec7738615994bd9f08fc98c1f3160c13977f
joshcoales <joshua@coales.co.uk>
2019-06-11 22:53:33 +0100

    Adding a welcome message

--- bot.py
+++ bot.py
@@ -36,7 +36,10 @@ class FASearchBot:
         dispatcher = updater.dispatcher
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-        start_handler = CommandHandler('beep', self.beep)
+        beep_handler = CommandHandler('beep', self.beep)
+        dispatcher.add_handler(beep_handler)
+
+        start_handler = CommandHandler('start', self.welcome_message)
         dispatcher.add_handler(start_handler)
 
         neaten_handler = MessageHandler(FilterRegex(self.FA_LINK), self.neaten_image)
@@ -48,6 +51,14 @@ class FASearchBot:
         while self.alive:
             print("Main thread alive")
             time.sleep(30)
+
+    def welcome_message(self, bot, update):
+        self.bot.send_message(
+            chat_id=update.message.chat_id,
+            text="Hello, I'm a new bot so I'm still learning. I can't do a whole lot yet. "
+                 "If you have any suggestions, requests, or questions, direct them to @deerspangle.\n"
+                 "For now, all I can do is neaten up any FA links you send me."
+        )
 
     def beep(self, bot, update):
         self.bot.send_message(chat_id=update.message.chat_id, text="boop")
