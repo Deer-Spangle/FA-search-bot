f3a2237cf613f55aeb9550ad6b31ccba3c38c977
joshcoales <joshua@coales.co.uk>
2019-06-12 20:22:22 +0100

    Adding first tests

---
+++ test/testWelcomeMessage.py
@@ -0,0 +1,21 @@
+import unittest
+
+from unittest.mock import patch
+import telegram
+
+from bot import FASearchBot
+from test.util.testTelegramUpdateObjects import MockTelegramUpdate
+
+
+class MockObjectsTest(unittest.TestCase):
+
+    @patch.object(telegram, "Bot")
+    def test_welcome_message(self, bot):
+        update = MockTelegramUpdate.with_command()
+        searchBot = FASearchBot("config.json")
+
+        searchBot.welcome_message(bot, update)
+
+        bot.send_message.assert_called()
+        assert bot.send_message.call_args[1]['chat_id'] == update.message.chat_id
+        assert "@deerspangle" in bot.send_message.call_args[1]['text']
---
+++ test/util/testTelegramUpdateObjects.py
@@ -0,0 +1,195 @@
+import unittest
+import uuid
+
+
+def generate_key():
+    return str(uuid.uuid4())
+
+
+class MockTelegramUpdate:
+
+    def __init__(self):
+        if self.__class__ == MockTelegramUpdate:
+            raise NotImplementedError()
+        self.message = None
+        self.callback_query = None
+
+    @staticmethod
+    def with_message(message_id=None, chat_id=None, text=None, text_markdown_urled=None):
+        return _MockTelegramMessage(
+            message_id=message_id,
+            chat_id=chat_id,
+            text=text,
+            text_markdown_urled=text_markdown_urled
+        )
+
+    @classmethod
+    def with_callback_query(cls, data=None):
+        return _MockTelegramCallback(
+            data=data
+        )
+
+    @classmethod
+    def with_command(cls, message_id=None, chat_id=None):
+        return _MockTelegramCommand(
+            message_id=message_id,
+            chat_id=chat_id
+        )
+
+
+class _MockTelegramMessage(MockTelegramUpdate):
+
+    def __init__(self, *, message_id=None, text=None, text_markdown_urled=None, chat_id=None):
+        super().__init__()
+        # Set up message data
+        self.message = _MockMessage(
+            message_id=message_id,
+            chat_id=chat_id,
+            text=text,
+            text_markdown_urled=text_markdown_urled
+        )
+
+    def with_photo(self, photo_file_id=None, caption=None):
+        self.message.set_photo(photo_file_id, caption)
+        return self
+
+    def with_document(self, file_id=None, mime_type=None):
+        self.message.set_document(file_id, mime_type)
+        return self
+
+
+class _MockTelegramCallback(MockTelegramUpdate):
+
+    def __init__(self, *, data=None):
+        super().__init__()
+        self.callback_query = _MockCallback(data)
+
+    def with_originating_message(self, message_id=None, chat_id=None):
+        self.callback_query.set_message(message_id, chat_id)
+        return self
+
+
+class _MockTelegramCommand(MockTelegramUpdate):
+
+    def __init__(self, *, message_id=None, chat_id=None):
+        super().__init__()
+        self.message = _MockMessage(
+            message_id=message_id,
+            chat_id=chat_id
+        )
+
+
+class _MockMessage:
+
+    def __init__(self, *, message_id=None, chat_id=None, text=None, text_markdown_urled=None):
+        self.message_id = message_id
+        self.chat_id = chat_id
+        self.text = text
+        self.text_markdown_urled = text_markdown_urled
+        # Set defaults
+        self.photo = []
+        self.caption = None
+        self.document = None
+        if message_id is None:
+            self.message_id = generate_key()
+        if chat_id is None:
+            self.chat_id = generate_key()
+        if text_markdown_urled is None:
+            self.text_markdown_urled = self.text
+
+    def set_photo(self, photo_file_id, caption):
+        # Defaults
+        if photo_file_id is None:
+            photo_file_id = generate_key()
+        # Set values
+        self.photo.append({"file_id": photo_file_id})
+        self.caption = caption
+
+    def set_document(self, file_id, mime_type):
+        self.document = _MockDocument(
+            file_id,
+            mime_type
+        )
+
+
+class _MockDocument:
+
+    def __init__(self, file_id=None, mime_type=None):
+        self.file_id = file_id
+        self.mime_type = mime_type
+        # Set defaults
+        if file_id is None:
+            self.file_id = generate_key()
+
+
+class _MockCallback:
+
+    def __init__(self, data=None):
+        self.data = data
+        self.message = None
+
+    def set_message(self, message_id, chat_id):
+        self.message = _MockMessage(message_id=message_id, chat_id=chat_id)
+
+
+class MockObjectsTest(unittest.TestCase):
+
+    def test_cannot_create_update(self):
+        try:
+            update = MockTelegramUpdate()
+            assert False, "Should have failed to create."
+        except NotImplementedError:
+            pass
+
+    def test_can_create_message(self):
+        update = MockTelegramUpdate.with_message()
+        assert update.callback_query is None
+        assert update.message is not None
+        assert update.message.message_id is not None
+        assert update.message.chat_id is not None
+        assert isinstance(update.message.photo, list)
+        assert len(update.message.photo) == 0
+
+    def test_can_create_message_with_photo(self):
+        update = MockTelegramUpdate.with_message().with_photo()
+        assert update.callback_query is None
+        assert update.message is not None
+        assert update.message.message_id is not None
+        assert update.message.chat_id is not None
+        assert isinstance(update.message.photo, list)
+        assert len(update.message.photo) == 1
+        assert update.message.photo[0]["file_id"] is not None
+
+    def test_can_create_message_with_document(self):
+        update = MockTelegramUpdate.with_message().with_document()
+        assert update.callback_query is None
+        assert update.message is not None
+        assert update.message.message_id is not None
+        assert update.message.chat_id is not None
+        assert isinstance(update.message.photo, list)
+        assert len(update.message.photo) == 0
+        assert update.message.document is not None
+        assert isinstance(update.message.document, _MockDocument)
+        assert update.message.document.file_id is not None
+        assert update.message.document.mime_type is None
+
+    def test_can_create_callback(self):
+        update = MockTelegramUpdate.with_callback_query()
+        assert update.message is None
+        assert update.callback_query is not None
+        assert update.callback_query.message is None
+
+    def test_can_create_callback_with_message(self):
+        update = MockTelegramUpdate.with_callback_query().with_originating_message()
+        assert update.message is None
+        assert update.callback_query is not None
+        assert update.callback_query.message is not None
+        assert update.callback_query.message.message_id is not None
+        assert update.callback_query.message.chat_id is not None
+
+    def test_can_create_command(self):
+        update = MockTelegramUpdate.with_command()
+        assert update.callback_query is None
+        assert update.message is not None
+        assert update.message.message_id is not None
+        assert update.message.chat_id is not None
