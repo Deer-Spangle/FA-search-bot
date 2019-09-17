ecfbabf7d21ac9dc6a5db348d9890e8fde812edd
joshcoales <joshua@coales.co.uk>
2019-09-17 15:50:01 +0100

    Implementing remaining tests for SubscriptionWatcher.run()

--- tests/test_subscription_watcher.py
+++ tests/test_subscription_watcher.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 import unittest
 from threading import Thread
@@ -9,6 +10,14 @@ from tests.util.mock_method import MockMethod
 
 
 class MockSubscription(Subscription):
+
+    def __init__(self, query, destination):
+        super().__init__(query, destination)
+        self.submissions_checked = []
+
+    def matches_result(self, result: FASubmissionFull) -> bool:
+        self.submissions_checked.append(result)
+        return True
 
     def send(self, result: FASubmissionFull):
         pass
@@ -49,16 +58,16 @@ class SubscriptionWatcherTest(unittest.TestCase):
 
     def test_run_calls_get_new_results(self):
         api = MockExportAPI()
-        s = SubscriptionWatcher(api)
+        watcher = SubscriptionWatcher(api)
         method_called = MockMethod([])
-        s._get_new_results = method_called.call
+        watcher._get_new_results = method_called.call
         # Shorten the wait
-        s.BACK_OFF = 1
+        watcher.BACK_OFF = 1
 
-        thread = Thread(target=lambda: self.watcher_killer(s))
+        thread = Thread(target=lambda: self.watcher_killer(watcher))
         thread.start()
         # Run watcher
-        s.run()
+        watcher.run()
         thread.join()
 
         assert method_called.called
@@ -66,35 +75,62 @@ class SubscriptionWatcherTest(unittest.TestCase):
     def test_run_checks_all_subscriptions(self):
         submission = MockSubmission("12322")
         api = MockExportAPI().with_submission(submission)
-        s = SubscriptionWatcher(api)
+        watcher = SubscriptionWatcher(api)
         method_called = MockMethod([MockSubmission])
-        s._get_new_results = method_called.call
-        s.BACK_OFF = 1
+        watcher._get_new_results = method_called.call
+        watcher.BACK_OFF = 1
         sub1 = MockSubscription("deer", 0)
-        sub1_called = MockMethod(True)
-        sub1.matches_result = sub1_called.call
         sub2 = MockSubscription("dog", 0)
-        sub2_called = MockMethod(True)
-        sub2.matches_result = sub2_called.call
-        s.subscriptions = [sub1, sub2]
+        watcher.subscriptions = [sub1, sub2]
 
-        thread = Thread(target=lambda: self.watcher_killer(s))
+        thread = Thread(target=lambda: self.watcher_killer(watcher))
         thread.start()
         # Run watcher
-        s.run()
+        watcher.run()
         thread.join()
 
-        assert sub1_called.called
-        assert sub2_called.called
+        assert submission in sub1.submissions_checked
+        assert submission in sub2.submissions_checked
         assert method_called.called
 
     def test_run_checks_all_new_results(self):
-        assert False
-        pass  # TODO
+        submission1 = MockSubmission("12322")
+        submission2 = MockSubmission("12324")
+        api = MockExportAPI().with_submissions([submission1, submission2])
+        watcher = SubscriptionWatcher(api)
+        method_called = MockMethod([MockSubmission])
+        watcher._get_new_results = method_called.call
+        watcher.BACK_OFF = 1
+        sub = MockSubscription("deer", 0)
+        watcher.subscriptions = [sub]
+
+        thread = Thread(target=lambda: self.watcher_killer(watcher))
+        thread.start()
+        # Run watcher
+        watcher.run()
+        thread.join()
+
+        assert submission1 in sub.submissions_checked
+        assert submission2 in sub.submissions_checked
+        assert method_called.called
 
     def test_run_sleeps_backoff_time(self):
-        assert False
-        pass  # TODO
+        api = MockExportAPI()
+        s = SubscriptionWatcher(api)
+        # Shorten the wait
+        s.BACK_OFF = 3
+
+        thread = Thread(target=lambda: self.watcher_killer(s))
+        thread.start()
+
+        # Run watcher
+        start_time = datetime.datetime.now()
+        s.run()
+        end_time = datetime.datetime.now()
+        thread.join()
+
+        time_waited = end_time - start_time
+        assert 3 <= time_waited.seconds <= 5
 
     def test_get_new_results_handles_empty_latest_ids(self):
         assert False
