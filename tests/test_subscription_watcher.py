5f469caf99b6d6bca71d38347eb1b7506f4bf26c
joshcoales <joshua@coales.co.uk>
2019-09-17 15:20:11 +0100

    Implementing first few SubscriptionWatcher tests

--- tests/test_subscription_watcher.py
+++ tests/test_subscription_watcher.py
@@ -1,19 +1,92 @@
+import time
 import unittest
+from threading import Thread
+
+from fa_submission import FASubmissionFull
+from subscription_watcher import SubscriptionWatcher, Subscription
+from tests.util.mock_export_api import MockExportAPI, MockSubmission
+from tests.util.mock_method import MockMethod
+
+
+class MockSubscription(Subscription):
+
+    def send(self, result: FASubmissionFull):
+        pass
 
 
 class SubscriptionWatcherTest(unittest.TestCase):
 
     def test_init(self):
-        pass  # TODO
+        api = MockExportAPI()
+        s = SubscriptionWatcher(api)
+
+        assert s.api == api
+        assert len(s.latest_ids) == 0
+        assert s.running is False
+        assert len(s.subscriptions) == 0
+
+    def watcher_killer(self, watcher: SubscriptionWatcher):
+        # Wait until watcher is running
+        while watcher.running is False:
+            time.sleep(0.1)
+        # Stop watcher
+        watcher.running = False
 
     def test_run_is_stopped_by_running_false(self):
-        pass  # TODO
+        api = MockExportAPI()
+        s = SubscriptionWatcher(api)
+        # Shorten the wait
+        s.BACK_OFF = 1
+
+        thread = Thread(target=lambda: self.watcher_killer(s))
+        thread.start()
+
+        # Run watcher
+        s.run()
+
+        assert True
+        thread.join()
 
     def test_run_calls_get_new_results(self):
-        pass  # TODO
+        api = MockExportAPI()
+        s = SubscriptionWatcher(api)
+        method_called = MockMethod([])
+        s._get_new_results = method_called.call
+        # Shorten the wait
+        s.BACK_OFF = 1
+
+        thread = Thread(target=lambda: self.watcher_killer(s))
+        thread.start()
+        # Run watcher
+        s.run()
+        thread.join()
+
+        assert method_called.called
 
     def test_run_checks_all_subscriptions(self):
-        pass  # TODO
+        submission = MockSubmission("12322")
+        api = MockExportAPI().with_submission(submission)
+        s = SubscriptionWatcher(api)
+        method_called = MockMethod([MockSubmission])
+        s._get_new_results = method_called.call
+        s.BACK_OFF = 1
+        sub1 = MockSubscription("deer", 0)
+        sub1_called = MockMethod(True)
+        sub1.matches_result = sub1_called.call
+        sub2 = MockSubscription("dog", 0)
+        sub2_called = MockMethod(True)
+        sub2.matches_result = sub2_called.call
+        s.subscriptions = [sub1, sub2]
+
+        thread = Thread(target=lambda: self.watcher_killer(s))
+        thread.start()
+        # Run watcher
+        s.run()
+        thread.join()
+
+        assert sub1_called.called
+        assert sub2_called.called
+        assert method_called.called
 
     def test_run_checks_all_new_results(self):
         assert False
