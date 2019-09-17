5dc8c9d6a24dbed02743e16e57c04dd3f416b634
joshcoales <joshua@coales.co.uk>
2019-09-17 15:14:57 +0100

    Test stubs should fail by default

--- tests/test_subscription.py
+++ tests/test_subscription.py
@@ -4,16 +4,21 @@ import unittest
 class SubscriptionTest(unittest.TestCase):
 
     def test_init(self):
+        assert False
         pass  # TODO
 
     def test_matches_result(self):
+        assert False
         pass  # TODO
 
     def test_to_json(self):
+        assert False
         pass  # TODO
 
     def test_from_json(self):
+        assert False
         pass  # TODO
 
     def test_to_json_and_back(self):
+        assert False
         pass  # TODO
--- tests/test_subscription_watcher.py
+++ tests/test_subscription_watcher.py
@@ -16,37 +16,49 @@ class SubscriptionWatcherTest(unittest.TestCase):
         pass  # TODO
 
     def test_run_checks_all_new_results(self):
+        assert False
         pass  # TODO
 
     def test_run_sleeps_backoff_time(self):
+        assert False
         pass  # TODO
 
     def test_get_new_results_handles_empty_latest_ids(self):
+        assert False
         pass  # TODO
 
     def test_get_new_results_updates_latest_ids(self):
+        assert False
         pass  # TODO
 
     def test_get_new_results_updates_latest_ids_after_checking_two_pages(self):
+        assert False
         pass  # TODO
 
     def test_get_new_results_returns_new_results(self):
+        assert False
         pass  # TODO
 
     def test_get_new_results_goes_to_another_page(self):
+        assert False
         pass  # TODO
 
     def test_get_new_results_respects_page_cap(self):
+        assert False
         pass  # TODO
 
     def test_update_latest_ids(self):
+        assert False
         pass  # TODO
 
     def test_to_json(self):
+        assert False
         pass  # TODO
 
     def test_from_json(self):
+        assert False
         pass  # TODO
 
     def test_to_json_and_back(self):
+        assert False
         pass  # TODO
