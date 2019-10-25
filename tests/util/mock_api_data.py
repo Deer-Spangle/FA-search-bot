eb64a6276019ea67094cdb995626c7ae05afa5f8
joshcoales <joshua@coales.co.uk>
2019-10-25 08:46:02 +0100

    Moving MockSubmission constructor stuff to MockAPIDataSubmission constructor

--- tests/util/mock_api_data.py
+++ tests/util/mock_api_data.py
@@ -1,7 +1,7 @@
 import random
 from abc import ABC
 
-from fa_submission import FAUser, FAUserShort
+from fa_submission import FAUser, FAUserShort, FASubmission
 from tests.util.mock_export_api import _random_string, _random_image_id
 
 
@@ -34,26 +34,70 @@ class MockAPIData(ABC):
 
 class MockAPIDataSubmission:
 
-    def __init__(self):
-        self.title = f"Example {_random_string()}"
-        self.description = _random_string() * 5
-        profile_name = _random_string()
-        self.author = FAUser.from_submission_dict({"name": profile_name.title(), "profile_name": profile_name})
-        self.sub_id = random.randint(10_000_000, 30_000_000)
-        self.image_id = _random_image_id(self.sub_id)
-        self.download_link = f"http://d.facdn.net/art/{self.author.profile_name}/1284661300/1284661300.fender_fender.png"
-        self.full_link = self.download_link
-        self.thumbnail_link = f"http://t.facdn.net/{self.sub_id}@400-{self.image_id}.jpg"
-        self.keywords = ["Example", "test"] + [_random_string() for _ in range(3)]
+    def __init__(
+            self,
+            submission_id: Union[str, int],
+            *,
+            username: str = None,
+            image_id: int = None,
+            file_size: int = 14852,
+            file_ext: str = "jpg",
+            fav_id: str = None,
+            title: str = None,
+            author: FAUser = None,
+            description: str = None,
+            keywords: List[str] = None
+    ):
+        # Internal variables
+        if image_id is None:
+            image_id = _random_image_id(int(submission_id))
+        if username is None:
+            username = _random_string()
+        if fav_id is None:
+            fav_id = str(_random_image_id(int(submission_id)))
+        folder = ""
+        if file_ext in FASubmission.EXTENSIONS_AUDIO:
+            folder = "music/"
+        if file_ext in FASubmission.EXTENSIONS_DOCUMENT:
+            folder = "stories/"
+        # Variables for superclass
+        thumbnail_url = f"https://t.facdn.net/{submission_id}@1600-{image_id}.jpg"
+        download_url = f"https://d.facdn.net/art/{username}/{folder}{image_id}/" \
+            f"{image_id}.{username}_{_random_string()}.{file_ext}"
+        if file_ext in FASubmission.EXTENSIONS_PHOTO + ["gif"]:
+            full_image_url = download_url
+        else:
+            full_image_url = download_url + ".jpg"
+        if title is None:
+            title = _random_string()
+        if author is None:
+            profile_name = _random_string()
+            author = FAUser.from_submission_dict({"name": profile_name.title(), "profile_name": profile_name})
+        if description is None:
+            description = _random_string() * 5
+        if keywords is None:
+            keywords = [_random_string() for _ in range(3)]
+        # Super
+        self.submission_id = submission_id
+        self.link = f"https://furaffinity.net/view/{submission_id}/"
+        self.thumbnail_url = thumbnail_url
+        self.title = title
+        self.author = author
+        self.download_url = download_url
+        self.full_image_url = full_image_url
+        self.description = description
+        self.keywords = keywords
+        self._download_file_size = None
+        self.fav_id = fav_id
+        self._download_file_size = file_size
 
-    def with_sub_id(self, sub_id):
-        self.sub_id = sub_id
-        self.image_id = _random_image_id(self.sub_id)
-        self.download_link = f"http://d.facdn.net/art/{self.author.profile_name}/1284661300/1284661300.fender_fender.png"
-        self.full_link = self.download_link
-        self.thumbnail_link = f"http://t.facdn.net/{self.sub_id}@400-{self.image_id}.jpg"
+    def build_submission(self):
+        pass  # TODO
 
-    def build(self):
+    def build_mock_submission(self):
+        pass  # TODO
+
+    def build_submission_json(self):
         return {
             "title": self.title,
             "description": self.description,
@@ -61,9 +105,12 @@ class MockAPIDataSubmission:
             "name": self.author.name,
             "profile": self.author.link,
             "profile_name": self.author.profile_name,
-            "link": f"https://www.furaffinity.net/view/{self.sub_id}/",
-            "download": self.download_link,
-            "full": self.full_link,
-            "thumbnail": self.thumbnail_link,
+            "link": f"https://www.furaffinity.net/view/{self.submission_id}/",
+            "download": self.download_url,
+            "full": self.full_image_url,
+            "thumbnail": self.thumbnail_url,
             "keywords": self.keywords
         }
+
+    def build_search_json(self):
+        pass  # TODO
