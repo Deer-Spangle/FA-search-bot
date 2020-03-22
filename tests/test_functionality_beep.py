ed717ddb43bc99589ab20957488b678eaf6e34df
Deer Spangle <deer@spangle.org.uk>
2020-03-22 00:45:37 +0000

    Adding pytest, swapping beep functionality to pytest, and passing in a mock context

--- tests/test_functionality_beep.py
+++ tests/test_functionality_beep.py
@@ -1,21 +1,27 @@
 import unittest
 
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+
+import pytest
 import telegram
 
 from functionalities.beep import BeepFunctionality
 from tests.util.mock_telegram_update import MockTelegramUpdate
 
 
-class BeepTest(unittest.TestCase):
+@pytest.fixture
+def context():
+    context = MagicMock()
+    context.bot = MagicMock()
+    return context
 
-    @patch.object(telegram, "Bot")
-    def test_beep(self, bot):
-        update = MockTelegramUpdate.with_command()
-        beep = BeepFunctionality()
 
-        beep.call(bot, update)
+def test_beep(context):
+    update = MockTelegramUpdate.with_command()
+    beep = BeepFunctionality()
 
-        bot.send_message.assert_called()
-        assert bot.send_message.call_args[1]['chat_id'] == update.message.chat_id
-        assert bot.send_message.call_args[1]['text'] == "boop"
+    beep.call(update, context)
+
+    context.bot.send_message.assert_called()
+    assert context.bot.send_message.call_args[1]['chat_id'] == update.message.chat_id
+    assert context.bot.send_message.call_args[1]['text'] == "boop"
