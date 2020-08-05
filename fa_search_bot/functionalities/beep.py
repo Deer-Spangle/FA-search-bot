23205a7b0432a8dc4bf3b2f040ae4f0ac566a0e1
Deer Spangle <deer@spangle.org.uk>
2020-08-05 08:23:17 +0100

    Adding logger calls to Beep functionality

--- fa_search_bot/functionalities/beep.py
+++ fa_search_bot/functionalities/beep.py
@@ -1,7 +1,13 @@
+import logging
+
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
 
 from fa_search_bot.functionalities.functionalities import BotFunctionality
+
+audit_logger = logging.getLogger("audit")
+usage_logger = logging.getLogger("usage")
+logger = logging.getLogger("fa_search_bot.functionalities.beep")
 
 
 class BeepFunctionality(BotFunctionality):
@@ -10,4 +16,7 @@ class BeepFunctionality(BotFunctionality):
         super().__init__(CommandHandler, command='beep')
 
     def call(self, update: Update, context: CallbackContext):
+        logger.info("Beep")
+        audit_logger.info("Beep function called in chat_id: %s", update.message.chat_id)
+        usage_logger.info("Beep function")
         context.bot.send_message(chat_id=update.message.chat_id, text="boop")
