88130d4dd3f4717c47dea558b2dc115563cd9d68
Deer Spangle <deer@spangle.org.uk>
2020-08-05 08:23:36 +0100

    Adding logger calls to ImageHashRecommend functionality

--- fa_search_bot/functionalities/image_hash_recommend.py
+++ fa_search_bot/functionalities/image_hash_recommend.py
@@ -1,9 +1,15 @@
+import logging
+
 import telegram
 from telegram import Chat
 from telegram.ext import MessageHandler, CallbackContext
 
 from fa_search_bot.filters import FilterImageNoCaption
 from fa_search_bot.functionalities.functionalities import BotFunctionality
+
+audit_logger = logging.getLogger("audit")
+usage_logger = logging.getLogger("usage")
+logger = logging.getLogger("fa_search_bot.functionalities.image_hash_recommend")
 
 
 class ImageHashRecommendFunctionality(BotFunctionality):
@@ -12,6 +18,9 @@ class ImageHashRecommendFunctionality(BotFunctionality):
         super().__init__(MessageHandler, filters=FilterImageNoCaption())
 
     def call(self, update: telegram.Update, context: CallbackContext):
+        logger.info("Recommending image hash bots")
+        audit_logger.info("Image hash recommend called in chat_id: %s", update.message.chat_id)
+        usage_logger.info("Image hash recommend")
         if update.message.chat.type == Chat.PRIVATE:
             context.bot.send_message(
                 chat_id=update.message.chat_id,
