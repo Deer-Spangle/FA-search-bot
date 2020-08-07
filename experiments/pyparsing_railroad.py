76f3304a75394c24b9cb2e621e88874b82659e14
Deer Spangle <deer@spangle.org.uk>
2020-08-07 12:31:22 +0100

    Moving railroad diagram into new docs/ directory and committing it

--- experiments/pyparsing_railroad.py
+++ experiments/pyparsing_railroad.py
@@ -8,8 +8,7 @@ from fa_search_bot.query_parser import query_parser
 def create_railroad_diagram() -> None:
     expr = query_parser()
     # Creating railroad diagrams
-    os.makedirs("experiments", exist_ok=True)
-    with open("experiments/output.html", "w") as fp:
+    with open("../docs/query_parser_railroad.html", "w") as fp:
         railroad = to_railroad(expr)
         fp.write(railroad_to_html(railroad))
 
