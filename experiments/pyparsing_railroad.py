d22e2a3cb43b5bc2881b66816c6b423f14d02afb
Deer Spangle <deer@spangle.org.uk>
2020-08-07 12:27:57 +0100

    Making the pyparsing railroad construction run when you run the pyparsing_railroad.py file

--- experiments/pyparsing_railroad.py
+++ experiments/pyparsing_railroad.py
@@ -1,3 +1,5 @@
+import os
+
 from pyparsing.diagram import to_railroad, railroad_to_html
 
 from fa_search_bot.query_parser import query_parser
@@ -6,6 +8,11 @@ from fa_search_bot.query_parser import query_parser
 def create_railroad_diagram() -> None:
     expr = query_parser()
     # Creating railroad diagrams
+    os.makedirs("experiments", exist_ok=True)
     with open("experiments/output.html", "w") as fp:
         railroad = to_railroad(expr)
         fp.write(railroad_to_html(railroad))
+
+
+if __name__ == "__main__":
+    create_railroad_diagram()
