ac7bd3bb16f40ca709a2aa98e4e5b9f8c42cdc90
Deer Spangle <deer@spangle.org.uk>
2020-11-04 23:09:19 +0000

    Bump minor version number for pause/resume subscription functionality, (and read version number from pyproject.toml)

--- fa_search_bot/_version.py
+++ fa_search_bot/_version.py
@@ -1,1 +1,6 @@
-__VERSION__ = "1.7.7"
+import tomlkit
+
+with open('../pyproject.toml') as pyproject:
+    file_contents = pyproject.read()
+
+__VERSION__ = tomlkit.parse(file_contents)['tool']['poetry']['version']
