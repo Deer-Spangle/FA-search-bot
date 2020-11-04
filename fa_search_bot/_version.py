b4c58a698f0721e9847ce6c17fe4a825ecce612b
Deer Spangle <deer@spangle.org.uk>
2020-11-04 23:12:23 +0000

    Fixing pyproject.toml path in _version.py

--- fa_search_bot/_version.py
+++ fa_search_bot/_version.py
@@ -1,6 +1,6 @@
 import tomlkit
 
-with open('../pyproject.toml') as pyproject:
+with open('./pyproject.toml') as pyproject:
     file_contents = pyproject.read()
 
 __VERSION__ = tomlkit.parse(file_contents)['tool']['poetry']['version']
