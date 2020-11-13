df5e57ee8c28f3e232c6606fee09d54d8c7aa8c7
Deer Spangle <deer@spangle.org.uk>
2020-11-13 21:51:19 +0000

    Adding test that subscriptions work after chat migration

--- fa_search_bot/tests/integration/test_subscriptions.py
+++ fa_search_bot/tests/integration/test_subscriptions.py
@@ -1,4 +1,6 @@
 import pytest
+from pyrogram.raw.functions.messages import MigrateChat
+from pyrogram.types import Chat
 from tgintegration import BotController
 
 pytestmark = pytest.mark.asyncio
@@ -95,5 +97,54 @@ async def test_block_commands(controller: BotController):
         await controller.send_command("list_blocklisted_tags")
     assert response.num_messages == 1
     assert response.messages[0].text.startswith("Current blocklist for this chat:")
+    lines = response.messages[0].text.html.split("\n")
+    assert "- test" not in lines
+
+
+async def test_group_migration(controller: BotController, group_chat: Chat):
+    # Create subscription
+    async with controller.collect(count=1, peer=group_chat.id) as response:
+        await controller.send_command("add_subscription", ["test"], peer=group_chat.id)
+    assert response.num_messages == 1
+    assert response.messages[0].text.startswith("Added subscription: \"test\".")
+    lines = response.messages[0].text.html.split("\n")
+    assert len(lines) >= 3
+    assert "Current subscriptions in this chat:" in lines
+    assert "- test" in lines
+
+    # List subscriptions
+    async with controller.collect(count=1, peer=group_chat.id) as response:
+        await controller.send_command("list_subscriptions", peer=group_chat.id)
+    assert response.num_messages == 1
+    assert response.messages[0].text.startswith("Current subscriptions in this chat:")
+    lines = response.messages[0].text.html.split("\n")
+    assert "- test" in lines
+
+    # Migrate chat to supergroup
+    updates = await controller.client.send(MigrateChat(chat_id=abs(group_chat.id)))
+    new_chat_id = int(f"-100{[chat.id for chat in updates.chats if chat.id != abs(group_chat.id)][0]}")
+    group_chat.id = new_chat_id
+
+    # List subscriptions
+    async with controller.collect(count=1, peer=new_chat_id) as response:
+        await controller.send_command("list_subscriptions", peer=new_chat_id)
+    assert response.num_messages == 1
+    assert response.messages[0].text.startswith("Current subscriptions in this chat:")
+    lines = response.messages[0].text.html.split("\n")
+    assert "- test" in lines
+
+    # Delete subscription
+    async with controller.collect(count=1, peer=new_chat_id) as response:
+        await controller.send_command("remove_subscription", ["test"], peer=new_chat_id)
+    assert response.num_messages == 1
+    assert response.messages[0].text.startswith("Removed subscription: \"test\".")
+    lines = response.messages[0].text.html.split("\n")
+    assert "- test" not in lines
+
+    # List subscriptions
+    async with controller.collect(count=1, peer=new_chat_id) as response:
+        await controller.send_command("list_subscriptions", peer=new_chat_id)
+    assert response.num_messages == 1
+    assert response.messages[0].text.startswith("Current subscriptions in this chat:")
     lines = response.messages[0].text.html.split("\n")
     assert "- test" not in lines
