b7b67b6dec8ecf8d00d468694ad507de168ed198
Deer Spangle <deer@spangle.org.uk>
2020-11-13 00:37:22 +0000

    Adding system test for blocklist commands

--- fa_search_bot/tests/integration/test_subscriptions.py
+++ fa_search_bot/tests/integration/test_subscriptions.py
@@ -1,12 +1,10 @@
 import pytest
 from tgintegration import BotController
 
-from fa_search_bot.bot import FASearchBot
-
 pytestmark = pytest.mark.asyncio
 
 
-async def test_subscription_commands(controller: BotController, bot: FASearchBot):
+async def test_subscription_commands(controller: BotController):
     # - create subscription, list subscriptions, remove subscription, list subscriptions
 
     async with controller.collect(count=1) as response:
@@ -67,4 +65,35 @@ async def test_subscription_commands(controller: BotController, bot: FASearchBot
     lines = response.messages[0].text.html.split("\n")
     assert "- test" not in lines
 
-# - create block, list blocks, remove block, list blocks
+
+async def test_block_commands(controller: BotController):
+    # - create block, list blocks, remove block, list blocks
+    async with controller.collect(count=1) as response:
+        await controller.send_command("add_blocklisted_tag", ["test"])
+    assert response.num_messages == 1
+    assert response.messages[0].text.startswith("Added tag to blocklist: \"test\".")
+    lines = response.messages[0].text.html.split("\n")
+    assert "Current blocklist for this chat:" in lines
+    assert "- test" in lines
+
+    async with controller.collect(count=1) as response:
+        await controller.send_command("list_blocklisted_tags")
+    assert response.num_messages == 1
+    assert response.messages[0].text.startswith("Current blocklist for this chat:")
+    lines = response.messages[0].text.html.split("\n")
+    assert "- test" in lines
+
+    async with controller.collect(count=1) as response:
+        await controller.send_command("remove_blocklisted_tag", ["test"])
+    assert response.num_messages == 1
+    assert response.messages[0].text.startswith("Removed tag from blocklist: \"test\".")
+    lines = response.messages[0].text.html.split("\n")
+    assert "Current blocklist for this chat:" in lines
+    assert "- test" not in lines
+
+    async with controller.collect(count=1) as response:
+        await controller.send_command("list_blocklisted_tags")
+    assert response.num_messages == 1
+    assert response.messages[0].text.startswith("Current blocklist for this chat:")
+    lines = response.messages[0].text.html.split("\n")
+    assert "- test" not in lines
