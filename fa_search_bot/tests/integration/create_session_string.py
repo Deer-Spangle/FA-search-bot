9f09e3ef763caa7b3ecdae3d4c79003fbab16c80
Deer Spangle <deer@spangle.org.uk>
2021-02-12 22:50:05 +0000

    Adding a helper task to generate a session string for integration tests

---
+++ fa_search_bot/tests/integration/create_session_string.py
@@ -0,0 +1,15 @@
+import os
+
+from pyrogram import Client
+
+
+if __name__ == "__main__":
+    client = Client(
+        "pyrogram",
+        api_id=os.getenv("CLIENT_API_ID"),
+        api_hash=os.getenv("CLIENT_API_HASH")
+    )
+
+    with client:
+        print(client.export_session_string())
+
