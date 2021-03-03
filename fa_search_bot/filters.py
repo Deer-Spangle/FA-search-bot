2307d7f34a4a1c362ef41150908f541337b70b3b
Deer Spangle <deer@spangle.org.uk>
2021-03-03 10:58:32 +0000

    I miss safe accessors

--- fa_search_bot/filters.py
+++ fa_search_bot/filters.py
@@ -1,13 +1,16 @@
+from telegram import Message
 from telegram.ext import Filters, BaseFilter
 
 
 class FilterRegex(Filters.regex):
 
-    def filter(self, message):
+    def filter(self, message: Message) -> bool:
         text = message.text_markdown_urled or message.caption_markdown_urled
         if text and self.pattern.search(text):
             return True
-        buttons = message.reply_markup.inline_keyboard or [[]]
+        buttons = [[]]
+        if message.reply_markup and message.reply_markup.inline_keyboard:
+            buttons = message.reply_markup.inline_keyboard
         for button_row in buttons:
             for button in button_row:
                 if button.text and self.pattern.search(button.text):
@@ -19,6 +22,6 @@ class FilterRegex(Filters.regex):
 
 class FilterImageNoCaption(BaseFilter):
 
-    def filter(self, message):
+    def filter(self, message: Message) -> bool:
         text = message.text_markdown_urled or message.caption_markdown_urled
         return not text and (bool(message.photo) or bool(message.document))
