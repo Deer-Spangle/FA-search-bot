73a3f795d8fe49341a0fcaf3ae102d102fdb87aa
Deer Spangle <deer@spangle.org.uk>
2021-03-03 09:18:34 +0000

    Making FilterRegex check the URL buttons as well as message text

--- fa_search_bot/filters.py
+++ fa_search_bot/filters.py
@@ -5,8 +5,15 @@ class FilterRegex(Filters.regex):
 
     def filter(self, message):
         text = message.text_markdown_urled or message.caption_markdown_urled
-        if text:
-            return bool(self.pattern.search(text))
+        if text and self.pattern.search(text):
+            return True
+        buttons = message.reply_markup.inline_keyboard or [[]]
+        for button_row in buttons:
+            for button in button_row:
+                if button.text and self.pattern.search(button.text):
+                    return True
+                if button.url and self.pattern.search(button.url):
+                    return True
         return False
 
 
