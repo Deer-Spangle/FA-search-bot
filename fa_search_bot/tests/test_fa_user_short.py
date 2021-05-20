bf66465c382fd89ede59fd097f1aa4aa6c6e9e43
Deer Spangle <deer@spangle.org.uk>
2021-05-20 23:28:17 +0100

    Splitting up test_fa_submission.py into separate files per class

---
+++ fa_search_bot/tests/test_fa_user_short.py
@@ -0,0 +1,16 @@
+import unittest
+
+from fa_search_bot.fa_submission import FAUserShort
+
+
+class FAUserShortTest(unittest.TestCase):
+
+    def test_constructor(self):
+        name = "John"
+        profile_name = "john"
+
+        author = FAUserShort(name, profile_name)
+
+        assert author.name == name
+        assert author.profile_name == profile_name
+        assert f"/user/{profile_name}" in author.link
