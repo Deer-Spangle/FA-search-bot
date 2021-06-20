b9dc570e47d2454b7b1c257a9452a6a106d47001
Deer Spangle <deer@spangle.org.uk>
2021-06-20 01:21:02 +0100

    Updating result IDs in inline neaten integration tests

--- fa_search_bot/tests/integration/telegram/test_inline_neaten.py
+++ fa_search_bot/tests/integration/telegram/test_inline_neaten.py
@@ -9,7 +9,7 @@ async def test_inline_id_query(controller: BotController):
     assert inline_results.results
     assert len(inline_results.results) == 1
     result = inline_results.results[0]
-    assert result.id == "19925704"
+    assert result.id == "fa:19925704"
     assert result.result.type == "photo"
     assert result.result.photo
 
@@ -19,7 +19,7 @@ async def test_inline_link_query(controller: BotController):
     assert inline_results.results
     assert len(inline_results.results) == 1
     result = inline_results.results[0]
-    assert result.id == "19925704"
+    assert result.id == "fa:19925704"
     assert result.result.type == "photo"
     assert result.result.photo
 
@@ -29,6 +29,6 @@ async def test_inline_e621_link_query(controller: BotController):
     assert inline_results.results
     assert len(inline_results.results) == 1
     result = inline_results.results[0]
-    assert result.id == "1092773"
+    assert result.id == "e6:1092773"
     assert result.result.type == "photo"
     assert result.result.photo
