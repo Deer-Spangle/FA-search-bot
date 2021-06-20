ae7443d27fdf084ff35a53e33f64da2d95f6f2be
Deer Spangle <deer@spangle.org.uk>
2021-06-20 16:08:18 +0100

    Making railroad diagram path a little more reliable

--- experiments/pyparsing_railroad.py
+++ experiments/pyparsing_railroad.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from pyparsing.diagram import to_railroad, railroad_to_html
 
@@ -8,7 +9,8 @@ from fa_search_bot.query_parser import query_parser
 def create_railroad_diagram() -> None:
     expr = query_parser()
     # Creating railroad diagrams
-    with open("../docs/query_parser_railroad.html", "w") as fp:
+    railroad_path = Path(__file__).parent.parent / "docs" / "query_parser_railroad.html"
+    with open(railroad_path, "w") as fp:
         railroad = to_railroad(expr)
         fp.write(railroad_to_html(railroad))
 
