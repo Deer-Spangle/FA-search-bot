740356d5440dfc413b5433f384e50e0be15dceff
Deer Spangle <deer@spangle.org.uk>
2021-06-20 19:19:26 +0100

    Close yippiclient properly in tests

--- fa_search_bot/tests/integration/sites/e621/conftest.py
+++ fa_search_bot/tests/integration/sites/e621/conftest.py
@@ -4,8 +4,8 @@ from yippi import AsyncYippiClient
 from fa_search_bot._version import __VERSION__
 
 
-@pytest.fixture  # (scope="session")
-def yippi_client():
+@pytest.fixture
+async def yippi_client():
     client = AsyncYippiClient("FASearchBot integration test", __VERSION__, "dr-spangle")
     yield client
-    client.close()
+    await client.close()
