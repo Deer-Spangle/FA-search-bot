35c6089d44736bf95abb3efa406fa9aa9b90529b
Deer Spangle <deer@spangle.org.uk>
2021-11-29 00:59:08 +0000

    Marking site integration tests as async

--- fa_search_bot/tests/integration/sites/fa/test_link_formats.py
+++ fa_search_bot/tests/integration/sites/fa/test_link_formats.py
@@ -1,21 +1,26 @@
+import pytest
+
 from fa_search_bot.sites.fa_handler import FAHandler
 
 
-def test_submission_link_format(api):
+@pytest.mark.asyncio
+async def test_submission_link_format(api):
     submission = await api.get_full_submission("19925704")
     handler = FAHandler(api)
 
     assert handler.FA_SUB_LINK.match(submission.link)
 
 
-def test_direct_link_format(api):
+@pytest.mark.asyncio
+async def test_direct_link_format(api):
     submission = await api.get_full_submission("19925704")
     handler = FAHandler(api)
 
     assert handler.FA_DIRECT_LINK.match(submission.download_url)
 
 
-def test_thumb_link_format(api):
+@pytest.mark.asyncio
+async def test_thumb_link_format(api):
     submission = await api.get_full_submission("19925704")
     handler = FAHandler(api)
 
