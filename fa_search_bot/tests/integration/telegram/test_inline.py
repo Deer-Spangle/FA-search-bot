1ae7402ca9265737c7266f85cb2941896ec74a8f
Deer Spangle <deer@spangle.org.uk>
2021-12-02 22:31:10 +0000

    Fixing test_inline_query integration test

--- fa_search_bot/tests/integration/telegram/test_inline.py
+++ fa_search_bot/tests/integration/telegram/test_inline.py
@@ -10,9 +10,11 @@ async def test_inline_query(controller: BotController):
     assert len(inline_results.results) > 10
     for result in inline_results.results:
         assert result.result.id
+        site_code, sub_id = result.result.id.split(":")
+        assert site_code == "fa"
         assert result.result.send_message.message
         assert "furaffinity.net/view/" in result.result.send_message.message
-        assert result.result.id in result.result.send_message.message
+        assert sub_id in result.result.send_message.message
         assert result.result.type == "photo"
 
 
