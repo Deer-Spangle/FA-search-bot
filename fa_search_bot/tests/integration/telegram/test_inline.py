c1e90fa5875819c694a63d08e625eaa1342537ff
Deer Spangle <deer@spangle.org.uk>
2021-12-02 22:31:26 +0000

    Adding telegram integration test for e621 searches

--- fa_search_bot/tests/integration/telegram/test_inline.py
+++ fa_search_bot/tests/integration/telegram/test_inline.py
@@ -28,3 +28,17 @@ async def test_inline_gallery_query(controller: BotController):
         assert "furaffinity.net/view/" in result.result.send_message.message
         assert result.result.id in result.result.send_message.message
         assert result.result.type == "photo"
+
+
+async def test_inline_e621_query(controller: BotController):
+    inline_results = await controller.query_inline("e621:deer")
+    assert inline_results.results
+    assert len(inline_results.results) > 10
+    for result in inline_results.results:
+        assert result.result.id
+        site_code, sub_id = result.result.id.split(":")
+        assert site_code == "e6"
+        assert result.result.send_message.message
+        assert "e621.net/posts/" in result.result.send_message.message
+        assert sub_id in result.result.send_message.message
+        assert result.result.type == "photo"
