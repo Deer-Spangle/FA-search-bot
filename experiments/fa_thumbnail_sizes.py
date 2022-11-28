b2ae3de1333453c64f2d381cab2b398112bc5b29
Deer Spangle <deer@spangle.org.uk>
2022-11-28 23:55:18 +0000

    Adding an experiment to explore which thumbnail sizes are valid now

---
+++ experiments/fa_thumbnail_sizes.py
@@ -0,0 +1,20 @@
+import requests
+
+####
+# This is an experiment to determine which thumbnail sizes are valid on FA, and which will redirect to a different size.
+# This seemed necessary as the bot currently (of writing) assumes FA will serve up a 1600px thumbnail, but tht seems
+# to redirect to 600px thumbnails now.
+###
+# Results (2022-11-28)
+# So, using this script, I have found that valid thumbnail sizes, in pixels are:
+# 50, 75, 100, 120, 150, 200, 250, 300, 320, 400, 600
+# There are no other valid thumbnail sizes under 2000 pixels
+###
+
+for size in range(5, 2000, 5):
+    thumb_url = f"https://t.furaffinity.net/19925704@{size}-1462827244.jpg"
+    resp = requests.head(thumb_url)
+    if resp.status_code == 200:
+        print(f"Valid thumbnail size: {size}")
+    else:
+        print(".", end="")
