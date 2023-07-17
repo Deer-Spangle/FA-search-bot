7d7ca80edc539f532f24bb5ee807b6469bb1e2a6
Deer-Spangle <deer@spangle.org.uk>
2023-07-17 10:01:21 +0100

    Adding metrics and observability to SubIDGatherer

--- fa_search_bot/subscriptions/sub_id_gatherer.py
+++ fa_search_bot/subscriptions/sub_id_gatherer.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import logging
 from typing import List, Optional
 
+import heartbeat
+from prometheus_client import Gauge, Counter
+
 from fa_search_bot.sites.furaffinity.fa_export_api import CloudflareError
 from fa_search_bot.sites.furaffinity.fa_submission import FASubmission
 from fa_search_bot.sites.submission_id import SubmissionID
@@ -10,15 +13,31 @@ from fa_search_bot.subscriptions.runnable import Runnable
 from fa_search_bot.subscriptions.utils import time_taken, _latest_submission_in_list
 
 logger = logging.getLogger(__name__)
-time_taken_listing_api = time_taken.labels(task="listing submissions to check")
-time_taken_waiting = time_taken.labels(task="waiting before re-checking")
+
+heartbeat.heartbeat_app_url = "https://heartbeat.spangle.org.uk/"
+heartbeat_app_name = "FASearchBot_task_SubIDGatherer"
+
+time_taken_listing_api = time_taken.labels(task="listing submissions to check", runnable="SubIDGatherer")
+time_taken_waiting = time_taken.labels(task="waiting before re-checking", runnable="SubIDGatherer")
+time_taken_publishing = time_taken.labels(task="publishing IDs to queues", runnable="SubIDGatherer")
+time_taken_updating_heartbeat = time_taken.labels(task="updating heartbeat", runnable="SubIDGatherer")
+latest_id_published = Gauge(
+    "fasearchbot_subidgatherer_latest_id_published_unixtime",
+    "UNIX timestamp that the latest submission ID got published to queues",
+)
+published_ID_counter = Counter(
+    "fasearchbot_subidgatherer_total_published_sub_id_count",
+    "Count of how many submission IDs have been published to queues",
+)
 
 
 class SubIDGatherer(Runnable):
     BROWSE_RETRY_BACKOFF = 20
     NEW_ID_BACKOFF = 20
+    UPDATE_PER_HEARTBEAT = 20
 
     async def run(self) -> None:
+        published_count = 0
         self.running = True
         while self.running:
             try:
@@ -29,8 +48,15 @@ class SubIDGatherer(Runnable):
                 continue
             for result in new_results:
                 sub_id = SubmissionID("fa", result.submission_id)
-                await self.watcher.wait_pool.add_sub_id(sub_id)
-                await self.watcher.fetch_data_queue.put(sub_id)
+                # Publish submission ID to queues for the other tasks
+                with time_taken_publishing.time():
+                    await self.watcher.wait_pool.add_sub_id(sub_id)
+                    await self.watcher.fetch_data_queue.put(sub_id)
+                # Metrics
+                published_ID_counter.inc()
+                latest_id_published.set_to_current_time()
+                self.heartbeat(published_count)
+            # Wait before checking for more
             with time_taken_waiting.time():
                 await self._wait_while_running(self.NEW_ID_BACKOFF)
 
@@ -76,3 +102,9 @@ class SubIDGatherer(Runnable):
                 logger.warning("FA is under cloudflare protection, waiting before retry")
                 await self._wait_while_running(self.BROWSE_RETRY_BACKOFF)
         return None
+
+    def heartbeat(self, published_count: int) -> None:
+        if published_count % self.UPDATE_PER_HEARTBEAT == 0:
+            with time_taken_updating_heartbeat.time():
+                heartbeat.update_heartbeat(heartbeat_app_name)
+            logger.debug("Heartbeat")
