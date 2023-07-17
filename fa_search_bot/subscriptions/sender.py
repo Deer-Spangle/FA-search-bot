7c53a9c3f34d6d9e6b71ab6396c7e9eb03792ea8
Deer-Spangle <deer@spangle.org.uk>
2023-07-17 22:00:18 +0100

    Adding more metrics to the Sender task, and reorganising the ones it has

--- fa_search_bot/subscriptions/sender.py
+++ fa_search_bot/subscriptions/sender.py
@@ -5,7 +5,6 @@ import datetime
 import logging
 from typing import Union, Dict, List
 
-import heartbeat
 from prometheus_client import Counter
 from telethon.errors import UserIsBlockedError, InputUserDeactivatedError, ChannelPrivateError, PeerIdInvalidError
 from telethon.tl.types import TypeInputPeer
@@ -18,42 +17,40 @@ from fa_search_bot.subscriptions.wait_pool import SubmissionCheckState
 
 logger = logging.getLogger(__name__)
 
-heartbeat.heartbeat_app_url = "https://heartbeat.spangle.org.uk/"
-heartbeat_app_name = "FASearchBot_sub_thread"
-
+time_taken_waiting = time_taken.labels(task="waiting for new events in queue")
 time_taken_sending_messages = time_taken.labels(task="sending messages to subscriptions")
 time_taken_saving_config = time_taken.labels(task="updating configuration")
-time_taken_updating_heartbeat = time_taken.labels(task="updating heartbeat")
-sub_updates = Counter("fasearchbot_fasubwatcher_updates_sent_total", "Number of subscription updates sent")
+sub_updates = Counter("fasearchbot_subscriptionsender_updates_sent_total", "Number of subscription updates sent")
 sub_blocked = Counter(
-    "fasearchbot_fasubwatcher_dest_blocked_total",
+    "fasearchbot_subscriptionsender_dest_blocked_total",
     "Number of times a destination has turned out to have blocked or deleted the bot without pausing subs first",
 )
 sub_update_send_failures = Counter(
-    "fasearchbot_fasubwatcher_updates_failed_total",
+    "fasearchbot_subscriptionsender_updates_failed_total",
     "Number of subscription updates which failed for unknown reason",
 )
+total_updates_sent = Counter(
+    "fasearchbot_subscriptionsender_messages_sent",
+    "Number of messages sent by the subscription sender",
+    labelnames=["media_type"],
+)
+updates_sent_cache = total_updates_sent.labels(media_type="cached")
+updates_sent_fresh_cache = total_updates_sent.labels(media_type="fresh_cache")
+updates_sent_upload = total_updates_sent.labels(media_type="upload")
+updates_sent_re_upload = total_updates_sent.labels(media_type="re_upload")
 
 
 class Sender(Runnable):
-    UPDATE_PER_HEARTBEAT = 10
 
-    async def run(self) -> None:
-        sent_count = 0
-        while self.running:
+    async def do_process(self) -> None:
+        with time_taken_waiting.time():
             next_state = await self.watcher.wait_pool.pop_next_ready_to_send()
-            # Send out messages
-            with time_taken_sending_messages.time():
-                await self._send_updates(next_state)
-            sent_count += 1
-            # Update latest ids with the submission we just checked, and save config
-            with time_taken_saving_config.time():
-                self.watcher.update_latest_id(next_state.sub_id)
-            # If we've done ten, update heartbeat
-            if sent_count % self.UPDATE_PER_HEARTBEAT == 0:
-                with time_taken_updating_heartbeat.time():
-                    heartbeat.update_heartbeat(heartbeat_app_name)
-                logger.debug("Heartbeat")
+        # Send out messages
+        with time_taken_sending_messages.time():
+            await self._send_updates(next_state)
+        # Update latest ids with the submission we just checked, and save config
+        with time_taken_saving_config.time():
+            self.watcher.update_latest_id(next_state.sub_id)
 
     async def _send_updates(self, state: SubmissionCheckState) -> None:
         subscriptions = state.matching_subscriptions
@@ -95,9 +92,19 @@ class Sender(Runnable):
     ) -> None:
         if state.cache_entry:
             if await state.cache_entry.try_to_send(self.watcher.client, chat, prefix=prefix):
+                updates_sent_cache.inc()
                 return
-            # TODO: Need to re-upload, instrument that
-        # Send message. (If uploaded media isn't set, it'll re-upload
+        # If uploaded media isn't set, check cache again
+        if not state.uploaded_media:
+            cache_entry = self.watcher.submission_cache.load_cache(state.sub_id)
+            if cache_entry:
+                if await state.cache_entry.try_to_send(self.watcher.client, chat, prefix=prefix):
+                    updates_sent_fresh_cache.inc()
+                    return
+            updates_sent_re_upload.inc()
+        else:
+            updates_sent_upload.inc()
+        # Send message
         result = await sendable.send_message(
             self.watcher.client,
             chat,
