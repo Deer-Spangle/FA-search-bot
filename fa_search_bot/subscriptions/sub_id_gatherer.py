20a79299f35d39fafca882ba2e256ff2f0bc0a99
Deer-Spangle <deer@spangle.org.uk>
2023-07-18 01:47:50 +0100

    SubIDGatherer needs to read the latest ID when starting, but then keep track of its own version to prevent stuffing the queue with duplicates

--- fa_search_bot/subscriptions/sub_id_gatherer.py
+++ fa_search_bot/subscriptions/sub_id_gatherer.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import logging
-from typing import List, Optional
+from typing import List, Optional, TYPE_CHECKING
 
 from prometheus_client import Counter
 
@@ -10,6 +10,9 @@ from fa_search_bot.sites.furaffinity.fa_submission import FASubmission
 from fa_search_bot.sites.submission_id import SubmissionID
 from fa_search_bot.subscriptions.runnable import Runnable
 from fa_search_bot.subscriptions.utils import time_taken, _latest_submission_in_list
+
+if TYPE_CHECKING:
+    from fa_search_bot.subscriptions.subscription_watcher import SubscriptionWatcher
 
 logger = logging.getLogger(__name__)
 
@@ -38,6 +41,12 @@ class SubIDGatherer(Runnable):
     BROWSE_RETRY_BACKOFF = 20
     NEW_ID_BACKOFF = 20
 
+    def __init__(self, watcher: "SubscriptionWatcher"):
+        super().__init__(watcher)
+        self.latest_recorded_id: Optional[int] = None
+        if self.watcher.latest_ids:
+            self.latest_recorded_id = int(self.watcher.latest_ids[-1])
+
     async def do_process(self) -> None:
         try:
             with time_taken_listing_api.time():
@@ -47,12 +56,11 @@ class SubIDGatherer(Runnable):
             return
         for result in new_results:
             sub_id = SubmissionID("fa", result.submission_id)
+            logger.debug("Publishing %s to queue and wait pool", sub_id)
             # Publish submission ID to queues for the other tasks
             with time_taken_publishing.time():
                 await self.watcher.wait_pool.add_sub_id(sub_id)
                 await self.watcher.fetch_data_queue.put(sub_id)
-            # Metrics
-            self.update_processed_metrics()
         # Wait before checking for more
         with time_taken_waiting.time():
             await self._wait_while_running(self.NEW_ID_BACKOFF)
@@ -61,20 +69,20 @@ class SubIDGatherer(Runnable):
         """
         Gets new results since last scan, returning them in order from oldest to newest.
         """
-        if len(self.watcher.latest_ids) == 0:
+        if self.latest_recorded_id is None:
             logger.info("First time checking subscriptions, getting initial submissions")
             newest_submission = await self._get_newest_submission()
             if not newest_submission:
                 return []
-            self.watcher.update_latest_id(SubmissionID("fa", newest_submission.submission_id))
+            self.latest_recorded_id = int(newest_submission.submission_id)
             return []
         newest_submission = await self._get_newest_submission()
         if not newest_submission:
             return []
         newest_id = int(newest_submission.submission_id)
-        latest_recorded_id = int(self.watcher.latest_ids[-1])
-        logger.info("Newest ID on FA: %s, latest recorded ID: %s", newest_id, latest_recorded_id)
-        new_results = [FASubmission(str(x)) for x in range(newest_id, latest_recorded_id, -1)]
+        logger.info("Newest ID on FA: %s, latest recorded ID: %s", newest_id, self.latest_recorded_id)
+        new_results = [FASubmission(str(x)) for x in range(newest_id, int(self.latest_recorded_id), -1)]
+        self.latest_recorded_id = newest_id
         logger.info("New submissions: %s", len(new_results))
         # Return oldest result first
         return new_results[::-1]
