c1ebf4aa6240385141bf9426ee5bbebdfc32bd21
Deer-Spangle <deer@spangle.org.uk>
2023-09-26 19:05:36 +0100

    If a submission has been looping and re-fetching for too long, send the submission without media

--- fa_search_bot/subscriptions/media_fetcher.py
+++ fa_search_bot/subscriptions/media_fetcher.py
@@ -9,10 +9,9 @@ from aiohttp import ClientPayloadError
 from prometheus_client import Counter
 
 from fa_search_bot.sites.furaffinity.sendable import SendableFASubmission
-from fa_search_bot.sites.sendable import UploadedMedia, DownloadError
-from fa_search_bot.sites.submission_id import SubmissionID
+from fa_search_bot.sites.sendable import UploadedMedia, DownloadError, SendSettings, CaptionSettings
 from fa_search_bot.subscriptions.runnable import Runnable, ShutdownError
-from fa_search_bot.subscriptions.utils import time_taken
+from fa_search_bot.subscriptions.utils import time_taken, TooManyRefresh
 
 logger = logging.getLogger(__name__)
 
@@ -56,17 +55,27 @@ class MediaFetcher(Runnable):
             with time_taken_uploading.time():
                 uploaded_media = await self.upload_sendable(sendable)
         except DownloadError as e:
-            if e.exc.status == 404:
-                await self.handle_deleted(sub_id)
+            if e.exc.status != 404:
+                raise e
+            uploaded_media = await self.handle_deleted(sendable)
+            if not uploaded_media:
                 return
-            raise e
         logger.debug("Upload complete for %s, publishing to wait pool", sub_id)
         with time_taken_publishing.time():
             await self.watcher.wait_pool.set_uploaded(sub_id, uploaded_media)
 
-    async def handle_deleted(self, sub_id: SubmissionID) -> None:
+    async def handle_deleted(self, sendable: SendableFASubmission) -> Optional[UploadedMedia]:
+        sub_id = sendable.submission_id
         logger.debug("Media for %s disappeared before it could be uploaded, throwing back to the fetch queue", sub_id)
-        await self.watcher.fetch_data_queue.put_refresh(sub_id)
+        try:
+            await self.watcher.fetch_data_queue.put_refresh(sub_id)
+        except TooManyRefresh:
+            logger.warning(
+                "Image could not be fetched for %s after %s refreshes. Sending without media",
+                sub_id,
+                self.watcher.fetch_data_queue.refresh_counter.refresh_limit
+            )
+            return UploadedMedia(sub_id, None, SendSettings(CaptionSettings(False, True, True, True), False, False))
 
     async def upload_sendable(self, sendable: SendableFASubmission) -> Optional[UploadedMedia]:
         while self.running:
